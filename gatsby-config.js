84354600be5137e1e0dbb6ca36cc40e43a15be87
‘lululutx’ <11223639+lululutx@user.noreply.gitee.com>
2023-07-24 16:14:59 +0800

    update my info

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,18 +3,14 @@
 module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
-    siteTitle: 'Running Page',
+    siteTitle: `LuLu's Running Page`,
     siteUrl: 'https://yihong.run',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-    description: 'Personal site and blog',
+    logo: 'https://avatars.githubusercontent.com/u/48202120?v=4',
+    description: '路在脚下',
     navLinks: [
       {
-        name: 'Blog',
-        url: 'https://yihong.run/running',
-      },
-      {
-        name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        name: 'gitbuh',
+        url: 'https://github.com/lululutx/',
       },
     ],
   },
