234bf5d4edad843e301a2a475dea60c0f567d1eb
‘lululutx’ <48202120+lululutx@users.noreply.github.com>
2023-10-09 09:45:21 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,18 +10,14 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
+  siteTitle: `LuLu's Running Page`,
   siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  logo: 'https://avatars.githubusercontent.com/u/48202120?v=4',
+  description: '路在脚下',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      name: 'github',
+      url: 'https://github.com/lululutx/',
     },
   ],
 };
